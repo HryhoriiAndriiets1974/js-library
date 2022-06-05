aba0ddd9894be1dff766d3e330d1f78b21308466
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-06-05 23:20:01 +0300

    error-simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // import './sass/main.scss';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // Описаний в документації
-import SimpleLightbox from "./index.js";
+// import SimpleLightbox from "./index.js";
 // Додатковий імпорт стилів
 // import "simplelightbox/dist/simple-lightbox.min.css";
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
 
 const axios = require('axios');
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: "alt",
-  captionDelay: 1000,
-});
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: "alt",
+//   captionDelay: 1000,
+// });
